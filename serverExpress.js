65dbc48552b7662e5ac3918f514e215aeb2ab4b7
STEPHAN CHRISTOPHER HOLLY <stephan@axiomcoding.com>
2017-08-01 14:22:02 -0700

    Send individual guest records from the database

--- serverExpress.js
+++ serverExpress.js
@@ -22,6 +22,25 @@ app.get('/guests', function(req,res) {
   });
 });
 
+app.get('/guests/:id', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var guests = JSON.parse(guestsJSON);
+
+    if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(guests[id]);
+  });
+});
+
 app.use(function(req,res) {
 
   res.sendStatus(404);
